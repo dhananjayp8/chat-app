a43973d1ee89d6bee5d83d0fa9aae92e1022c2b5
Dhananjay Puranik <dhananjaypuranik8@gmail.com>
2024-04-17 14:20:38 +0530

    created join chat form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,16 @@ import io from "socket.io-client";
 
 const socket = io.connect("http://localhost:2000");
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <div className="join_room">
+        <h1>Join Chat</h1>
+        <input type="text" />
+        <input type="text" />
+        <button>Join</button>
+      </div>
+    </>
+  );
 };
 
 export default App;
